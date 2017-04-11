daeea7fdcf774efde48d67b59e3b65c38e385722
Kyle Tolle <kyle@nullsix.com>
2017-04-10 22:01:58 -0700

    Make two more methods public

--- lib/everything/blog/post.rb
+++ lib/everything/blog/post.rb
@@ -10,6 +10,21 @@ module Everything
         @post_name = post_name
       end
 
+      def created_at
+        timestamp_to_use = piece.metadata['created_at'] || piece.metadata['wordpress']['post_date']
+        Time.at(timestamp_to_use)
+      end
+
+      def created_on
+        # Formatted like: June 24, 2016
+        created_at.strftime('%B %d, %Y')
+      end
+
+      def created_on_iso8601
+        # Formatted like: 2016-06-24
+        created_at.strftime('%F')
+      end
+
       def public?
         return false unless piece && File.exist?(piece.metadata.file_path)
 
@@ -24,18 +39,12 @@ module Everything
         Dir.glob(media_glob)
       end
 
+      def media_glob
+        File.join(piece.full_path, '*.{jpg,png,gif,mp3}')
+      end
+
       def piece
         @piece ||= Everything::Piece.find_by_name_recursive(post_name)
-      end
-
-      def created_on
-        # Formatted like: June 24, 2016
-        created_at.strftime('%B %d, %Y')
-      end
-
-      def created_on_iso8601
-        # Formatted like: 2016-06-24
-        created_at.strftime('%F')
       end
 
       def_delegators :piece, :name, :title, :body
@@ -43,15 +52,6 @@ module Everything
     private
 
       attr_reader :post_name
-
-      def created_at
-        timestamp_to_use = piece.metadata['created_at'] || piece.metadata['wordpress']['post_date']
-        Time.at(timestamp_to_use)
-      end
-
-      def media_glob
-        File.join(piece.full_path, '*.{jpg,png,gif,mp3}')
-      end
     end
   end
 end
