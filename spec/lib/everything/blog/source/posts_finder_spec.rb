11a579a80b0d74be0e261fb2214f439cdb10805a
Kyle Tolle <kyle@nullsix.com>
2017-07-27 21:52:54 -0700

    Move the requires around

--- spec/lib/everything/blog/source/posts_finder_spec.rb
+++ spec/lib/everything/blog/source/posts_finder_spec.rb
@@ -2,9 +2,9 @@ require 'pp' # Helps prevent an error like: 'superclass mismatch for class File'
 require 'bundler/setup'
 Bundler.require(:default)
 require './lib/everything/blog/source/posts_finder'
+require 'fakefs/spec_helpers'
 require './spec/support/shared'
 require './spec/support/post_helpers'
-require 'fakefs/spec_helpers'
 
 describe Everything::Blog::Source::PostsFinder do
   include FakeFS::SpecHelpers
