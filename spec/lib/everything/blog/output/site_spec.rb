9ba8ffb9e9c1cf8829d42fe34d1b9dea030c04ad
Kyle Tolle <kyle@nullsix.com>
2019-01-11 19:53:35 -0500

    Add some pending specs

---
+++ spec/lib/everything/blog/output/site_spec.rb
@@ -0,0 +1,7 @@
+require './lib/everything/blog/output/site'
+
+describe Everything::Blog::Output::Site do
+  describe '#initialize'
+  describe '#generate'
+  describe '#output_files'
+end
---
+++ spec/lib/everything/blog/output/stylesheet_spec.rb
@@ -0,0 +1,12 @@
+require './lib/everything/blog/output/stylesheet'
+
+describe Everything::Blog::Output::Stylesheet do
+  describe '#initialize'
+  describe '#source_file'
+  describe '#output_file_name'
+  describe '#output_dir_path'
+  describe '#output_file_path'
+  describe '#relative_dir_path'
+  describe '#save_file'
+  describe '#should_generate_output?'
+end
