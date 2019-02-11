3c7a7d8130a91fb32268be521804c4d84463bf15
Kyle Tolle <kyle@nullsix.com>
2019-02-10 20:10:56 -0700

    Add specs for output stylesheet #initialize

--- spec/lib/everything/blog/output/stylesheet_spec.rb
+++ spec/lib/everything/blog/output/stylesheet_spec.rb
@@ -2,13 +2,28 @@ require 'pp' # Helps prevent an error like: 'superclass mismatch for class File'
 require 'bundler/setup'
 Bundler.require(:default)
 require './lib/everything/blog/output/stylesheet'
+require './spec/support/shared'
 
 describe Everything::Blog::Output::Stylesheet do
+  include_examples 'with fake stylesheet'
+
+  let(:given_source_stylesheet) do
+    Everything::Blog::Source::Stylesheet.new
+  end
   let(:stylesheet) do
-    described_class.new(source_stylesheet)
+    described_class.new(given_source_stylesheet)
   end
 
-  describe '#initialize'
+  describe '#initialize' do
+    it 'takes a source_file argument' do
+      expect { stylesheet }.not_to raise_error
+    end
+
+    it 'sets the source_file attr' do
+      expect(stylesheet.source_file).to eq(given_source_stylesheet)
+    end
+  end
+
   describe '#source_file'
   describe '#output_file_name'
   describe '#output_dir_path'
