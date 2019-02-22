65ff20c0bcea3eced478563419f0d3d8b99282d8
Kyle Tolle <kyle@nullsix.com>
2019-02-21 22:25:47 -0700

    Add implementation and specs for FogConfig

--- spec/lib/everything/blog/fog_config_spec.rb
+++ spec/lib/everything/blog/fog_config_spec.rb
@@ -2,54 +2,136 @@ require 'spec_helper'
 require './lib/everything/blog/fog_config'
 
 describe Everything::Blog::FogConfig do
+  let(:fog_config) do
+    described_class.new
+  end
+
+  let(:fake_env_value) { 'fake_env_value' }
+
+  shared_context 'with fake aws_access_key_id env var' do
+    before do
+      ENV['AWS_ACCESS_KEY_ID'] = fake_env_value
+    end
+  end
+
+  shared_context 'with fake aws_secret_access_key env var' do
+    before do
+      ENV['AWS_SECRET_ACCESS_KEY'] = fake_env_value
+    end
+  end
+
+  shared_context 'with fake aws_storage_bucket env var' do
+    before do
+      ENV['AWS_STORAGE_BUCKET'] = fake_env_value
+    end
+  end
+
+  shared_context 'with fake aws_storage_region env var' do
+    before do
+      ENV['AWS_STORAGE_REGION'] = fake_env_value
+    end
+  end
+
   describe '#to_h' do
+    include_context 'with fake aws_access_key_id env var'
+    include_context 'with fake aws_secret_access_key env var'
+    include_context 'with fake aws_storage_bucket env var'
+    include_context 'with fake aws_storage_region env var'
+
     describe 'provider key' do
-      it 'is included'
-      it 'matches the #provider value'
+      it 'is included' do
+        expect(fog_config.to_h).to have_key(:provider)
+      end
+
+      it 'matches the #provider value' do
+        expect(fog_config.to_h[:provider]).to eq(fog_config.provider)
+      end
     end
 
     describe 'aws_access_key_id key' do
-      it 'is included'
-      it 'matches the #aws_access_key_id value'
+      it 'is included' do
+        expect(fog_config.to_h).to have_key(:aws_access_key_id)
+      end
+
+      it 'matches the #aws_access_key_id value' do
+        expect(fog_config.to_h[:aws_access_key_id])
+          .to eq(fog_config.aws_access_key_id)
+      end
     end
 
     describe 'aws_secret_access_key key' do
-      it 'is included'
-      it 'matches the #aws_secret_access_key value'
+      it 'is included' do
+        expect(fog_config.to_h).to have_key(:aws_secret_access_key)
+      end
+
+      it 'matches the #aws_secret_access_key value' do
+        expect(fog_config.to_h[:aws_secret_access_key])
+          .to eq(fog_config.aws_secret_access_key)
+      end
     end
 
     describe 'region key' do
-      it 'is included'
-      it 'matches the #region value'
+      it 'is included' do
+        expect(fog_config.to_h).to have_key(:region)
+      end
+
+      it 'matches the #aws_storage_region value' do
+        expect(fog_config.to_h[:region]).to eq(fog_config.aws_storage_region)
+      end
     end
 
     describe 'path_style key' do
-      it 'is included'
-      it 'matches the #path_style value'
+      it 'is included' do
+        expect(fog_config.to_h).to have_key(:path_style)
+      end
+
+      it 'matches the #path_style value' do
+        expect(fog_config.to_h[:path_style]).to eq(fog_config.path_style)
+      end
     end
   end
 
   describe '#provider' do
-    it "is 'AWS'"
+    it "is 'AWS'" do
+      expect(fog_config.provider).to eq('AWS')
+    end
   end
 
   describe '#aws_access_key_id' do
-    it 'is the value of the matching environment variable'
+    include_context 'with fake aws_access_key_id env var'
+
+    it 'is the value of the matching environment variable' do
+      expect(fog_config.aws_access_key_id).to eq(fake_env_value)
+    end
   end
 
   describe '#aws_secret_access_key' do
-    it 'is the value of the matching environment variable'
+    include_context 'with fake aws_secret_access_key env var'
+
+    it 'is the value of the matching environment variable' do
+      expect(fog_config.aws_secret_access_key).to eq(fake_env_value)
+    end
   end
 
   describe '#aws_storage_bucket' do
-    it 'is the value of the matching environment variable'
+    include_context 'with fake aws_storage_bucket env var'
+
+    it 'is the value of the matching environment variable' do
+      expect(fog_config.aws_storage_bucket).to eq(fake_env_value)
+    end
   end
 
-  describe '#region' do
-    it 'is the value of the matching environment variable'
+  describe '#aws_storage_region' do
+    include_context 'with fake aws_storage_region env var'
+
+    it 'is the value of the matching environment variable' do
+      expect(fog_config.aws_storage_region).to eq(fake_env_value)
+    end
   end
 
   describe '#path_style' do
-    it 'is true'
+    it 'is true' do
+      expect(fog_config.path_style).to eq(true)
+    end
   end
 end
