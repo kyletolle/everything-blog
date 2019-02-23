8610bd1bbd2488cbf52f90edb29da5530cec5c55
Kyle Tolle <kyle@nullsix.com>
2019-02-23 15:50:10 -0700

    Use FogConfig in S3Bucket

--- lib/everything/blog/s3_bucket.rb
+++ lib/everything/blog/s3_bucket.rb
@@ -1,3 +1,5 @@
+require_relative 'fog_config'
+
 module Everything
   class Blog
     class S3Bucket
@@ -6,43 +8,18 @@ module Everything
       end
 
       def bucket
-        @bucket ||= s3_connection.directories.get(aws_storage_bucket)
+        @bucket ||= s3_connection.directories
+          .get(fog_config.aws_storage_bucket)
       end
 
       def s3_connection
-        Fog::Storage.new(fog_config)
+        Fog::Storage.new(fog_config.to_h)
       end
 
     private
 
       def fog_config
-        {
-          provider:              fog_provider,
-          aws_access_key_id:     aws_access_key_id,
-          aws_secret_access_key: aws_secret_access_key,
-          region:                aws_storage_region,
-          path_style:            true
-        }
-      end
-
-      def fog_provider
-        'AWS'
-      end
-
-      def aws_access_key_id
-        Fastenv.aws_access_key_id
-      end
-
-      def aws_secret_access_key
-        Fastenv.aws_secret_access_key
-      end
-
-      def aws_storage_region
-        Fastenv.aws_storage_region
-      end
-
-      def aws_storage_bucket
-        Fastenv.aws_storage_bucket
+        FogConfig.new
       end
     end
   end
