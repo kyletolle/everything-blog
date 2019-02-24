2fcf31230aec01d66a62ae7c730a479a84bf3837
Kyle Tolle <kyle@nullsix.com>
2019-02-23 17:13:11 -0700

    Start adding S3Site.ToRemoteFile specs

--- lib/everything/blog/s3_site.rb
+++ lib/everything/blog/s3_site.rb
@@ -1,3 +1,7 @@
+require_relative 'output/stylesheet'
+require_relative 'output/index'
+require_relative 'output/media'
+require_relative 'output/page'
 require_relative 'remote/html_file'
 require_relative 'remote/stylesheet_file'
 require_relative 'remote/binary_file'
@@ -5,16 +9,20 @@ require_relative 'remote/binary_file'
 module Everything
   class Blog
     class S3Site
-      def self.ToRemoteFile(file)
-        case file
-        when Output::Stylesheet
-          Everything::Blog::Remote::StylesheetFile.new(file)
-        when Output::Index
-          Everything::Blog::Remote::HtmlFile.new(file)
-        when Output::Media
-          Everything::Blog::Remote::BinaryFile.new(file)
-        when Output::Page
-          Everything::Blog::Remote::HtmlFile.new(file)
+      def self.ToRemoteFile(output_file)
+        case output_file
+        when Everything::Blog::Output::Stylesheet
+          Everything::Blog::Remote::StylesheetFile.new(output_file)
+        when Everything::Blog::Output::Index
+          Everything::Blog::Remote::HtmlFile.new(output_file)
+        when Everything::Blog::Output::Media
+          Everything::Blog::Remote::BinaryFile.new(output_file)
+        when Everything::Blog::Output::Page
+          Everything::Blog::Remote::HtmlFile.new(output_file)
+        else
+          raise Everything::Blog::Remote::NoRemoteFileTypeFound,
+            'No corresponding Remote class found for output file type ' \
+            "`#{output_file.class.name}`"
         end
       end
 
--- spec/lib/everything/blog/s3_site_spec.rb
+++ spec/lib/everything/blog/s3_site_spec.rb
@@ -5,5 +5,22 @@ require 'spec_helper'
 require './lib/everything/blog/s3_site'
 
 describe Everything::Blog::S3Site do
+  describe '.ToRemoteFile' do
+    let(:remote_file) do
+      described_class.ToRemoteFile(given_output_file)
+    end
 
+    context 'with no output file' do
+      let(:given_output_file) do
+        'Not a file at all'
+      end
+
+      it 'raises the correct error and message' do
+        expect { remote_file }.to raise_error(
+          Everything::Blog::Remote::NoRemoteFileTypeFound,
+          /No .* Remote class found .*String/
+        )
+      end
+    end
+  end
 end
