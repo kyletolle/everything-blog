6b347da339a45f73398e06efc6dc1866395642e8
Kyle Tolle <kyle@nullsix.com>
2019-03-03 00:06:45 -0700

    Create a couple specs for Remote::FileBase

--- spec/lib/everything/blog/remote/file_base_spec.rb
+++ spec/lib/everything/blog/remote/file_base_spec.rb
@@ -1,4 +1,34 @@
+require 'pp' # Helps prevent an error like: 'superclass mismatch for class File'
+require 'bundler/setup'
+Bundler.require(:default)
 require './lib/everything/blog/remote/file_base'
 
 describe Everything::Blog::Remote::FileBase do
+  context '#content' do
+    context 'when the class is used without going through a child class' do
+      subject { file_base_instance.content }
+
+      let(:file_base_instance) do
+        described_class.new(nil)
+      end
+
+      it 'raises a NotImplementedError' do
+        expect { subject }.to raise_error(NotImplementedError)
+      end
+    end
+  end
+
+  context '#content_type' do
+    context 'when the class is used without going through a child class' do
+      subject { file_base_instance.content_type }
+
+      let(:file_base_instance) do
+        described_class.new(nil)
+      end
+
+      it 'raises a NotImplementedError' do
+        expect { subject }.to raise_error(NotImplementedError)
+      end
+    end
+  end
 end
