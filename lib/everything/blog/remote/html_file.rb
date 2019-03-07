363fd91af3b70c8fca21c298e97fe736592c1581
Kyle Tolle <kyle@nullsix.com>
2019-03-06 22:16:10 -0700

    Start adding specs for remote html file

--- lib/everything/blog/remote/html_file.rb
+++ lib/everything/blog/remote/html_file.rb
@@ -4,12 +4,14 @@ module Everything
   class Blog
     module Remote
       class HtmlFile < Everything::Blog::Remote::FileBase
+        HTML_CONTENT_TYPE = 'text/html'
+
         def content
-          output_file.content
+          output_file.output_content
         end
 
         def content_type
-          'text/html'
+          HTML_CONTENT_TYPE
         end
       end
     end
--- spec/lib/everything/blog/remote/html_file_spec.rb
+++ spec/lib/everything/blog/remote/html_file_spec.rb
@@ -2,13 +2,50 @@ require 'pp' # helps prevent an error like: 'superclass mismatch for class file'
 require 'bundler/setup'
 Bundler.require(:default)
 require './lib/everything/blog/remote/html_file'
+require './lib/everything/blog/output/index'
+require './spec/support/shared'
 
 describe Everything::Blog::Remote::HtmlFile do
   # TODO: Add specs for these
   # include_context 'behaves like a Remote::FileBase'
+  include_context 'with fake output index'
 
-  describe '#initialize'
-  describe '#content'
-  describe '#content_type'
+  let(:html_file) do
+    described_class.new(given_output_file)
+  end
+  let(:given_output_file) do
+    fake_output_index
+  end
+
+  describe '::HTML_CONTENT_TYPE' do
+    it 'is text/html' do
+      expect(described_class::HTML_CONTENT_TYPE).to eq('text/html')
+    end
+  end
+
+  describe '#initialize' do
+    it 'sets the output_file to the given output file' do
+      expect(html_file.output_file).to eq(given_output_file)
+    end
+  end
+
+  describe '#content' do
+    before do
+      allow(given_output_file)
+        .to receive(:output_content)
+        .and_return("This content's completely fake and should not be trusted.")
+    end
+
+    it "is the output file's content" do
+      expect(html_file.content).to eq(given_output_file.output_content)
+    end
+  end
+
+  describe '#content_type' do
+    it 'is equal to HTML_CONTENT_TYPE' do
+      expect(html_file.content_type).to eq(described_class::HTML_CONTENT_TYPE)
+    end
+  end
+
   describe '#send'
 end
