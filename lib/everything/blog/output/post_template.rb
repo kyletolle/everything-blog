ffbb288678caa8be8017f1e4eef12646950f9270
Kyle Tolle <kyle@nullsix.com>
2019-03-20 18:13:41 -0600

    Fully qualify template base class

--- lib/everything/blog/output/post_template.rb
+++ lib/everything/blog/output/post_template.rb
@@ -3,7 +3,7 @@ require_relative 'template_base'
 module Everything
   class Blog
     module Output
-      class PostTemplate < TemplateBase
+      class PostTemplate < Everything::Blog::Output::TemplateBase
         TEMPLATE_NAME = 'post.html.erb'
 
         def template_name
