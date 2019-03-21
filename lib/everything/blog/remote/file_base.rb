6db1f2ced2a6a4e6e4f63fefeaaf9855f30e9779
Kyle Tolle <kyle@nullsix.com>
2019-03-20 21:08:06 -0600

    Comment out currently unused code in Remote::FileBase

--- lib/everything/blog/remote/file_base.rb
+++ lib/everything/blog/remote/file_base.rb
@@ -20,51 +20,51 @@ module Everything
           raise NotImplementedError
         end
 
-        def send
-          if file_does_not_exist || local_file_is_newer
-            puts "SENDING TO S3 IS FAKED OUT RIGHT NOW"
-            # create_remote_file
-          end
-        end
+        # def send
+        #   if file_does_not_exist || local_file_is_newer
+        #     puts "SENDING TO S3 IS FAKED OUT RIGHT NOW"
+        #     # create_remote_file
+        #   end
+        # end
 
       private
 
-        def file_does_not_exist
-          remote_file.nil?
-        end
+        # def file_does_not_exist
+        #   remote_file.nil?
+        # end
 
-        def local_file_is_newer
-          remote_file&.etag != content_hash
-        end
+        # def local_file_is_newer
+        #   remote_file&.etag != content_hash
+        # end
 
-        def create_remote_file
-          s3_bucket.files.create(
-            key: remote_key,
-            body: content,
-            content_type: content_type
-          )
-        end
+        # def create_remote_file
+        #   s3_bucket.files.create(
+        #     key: remote_key,
+        #     body: content,
+        #     content_type: content_type
+        #   )
+        # end
 
-        def remote_key
-          output_file.relative_dir_path
-            .tap {|o| puts "OUTPUT FILE RELATIVE PATH: #{o}"}
-        end
+        # def remote_key
+        #   output_file.relative_dir_path
+        #     .tap {|o| puts "OUTPUT FILE RELATIVE PATH: #{o}"}
+        # end
 
-        def content_hash
-          md5.hexdigest(content)
-        end
+        # def content_hash
+        #   md5.hexdigest(content)
+        # end
 
-        def remote_file
-          s3_bucket.files.head(remote_key)
-        end
+        # def remote_file
+        #   s3_bucket.files.head(remote_key)
+        # end
 
-        def s3_bucket
-          @s3_bucket ||= Everything::Blog::S3Bucket.new
-        end
+        # def s3_bucket
+        #   @s3_bucket ||= Everything::Blog::S3Bucket.new
+        # end
 
-        def md5
-          @md5 ||= Digest::MD5.new
-        end
+        # def md5
+        #   @md5 ||= Digest::MD5.new
+        # end
       end
     end
   end
