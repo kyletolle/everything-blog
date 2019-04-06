100ed4c364aad8f3066e2eea6e125536ff8bca7e
Kyle Tolle <kyle@nullsix.com>
2019-04-05 18:44:23 -0600

    Add specs for Remote::StylesheetFile#content_hash

--- spec/lib/everything/blog/remote/stylesheet_file_spec.rb
+++ spec/lib/everything/blog/remote/stylesheet_file_spec.rb
@@ -2,10 +2,12 @@ require 'pp' # helps prevent an error like: 'superclass mismatch for class file'
 require 'bundler/setup'
 Bundler.require(:default)
 require './lib/everything/blog/remote/stylesheet_file'
+require './lib/everything/blog/output/stylesheet'
 require './spec/support/shared'
 
 describe Everything::Blog::Remote::StylesheetFile do
   include_context 'with fake output stylesheet'
+  include_context 'with mock fog'
 
   let(:stylesheet_file) do
     described_class.new(given_output_file)
@@ -35,6 +37,37 @@ describe Everything::Blog::Remote::StylesheetFile do
 
     it "is the output's file's content" do
       expect(stylesheet_file.content).to eq(given_output_file.output_content)
+    end
+  end
+
+  describe '#content_hash' do
+    subject { stylesheet_file.content_hash }
+
+    let(:md5_double) { instance_double(Digest::MD5) }
+
+    before do
+      allow(Digest::MD5)
+        .to receive(:new)
+        .and_return(md5_double)
+      allow(md5_double)
+        .to receive(:hexdigest)
+    end
+
+    it 'uses the md5 library to do a hexdigest' do
+      subject
+
+      expect(md5_double)
+        .to have_received(:hexdigest)
+        .with(stylesheet_file.content)
+    end
+
+    it 'returns the md5 hash' do
+      fake_hash = 'defn0tah4sh'
+
+      allow(md5_double)
+        .to receive(:hexdigest)
+        .and_return(fake_hash)
+      expect(subject).to eq(fake_hash)
     end
   end
 
