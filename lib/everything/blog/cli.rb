683aa0e3867bcc8f8b6a5cd39d724b12528f8895
Kyle Tolle <kyle@nullsix.com>
2019-07-03 18:45:30 -0600

    Add verbose logging to everything/blog/cli

--- lib/everything/blog/cli.rb
+++ lib/everything/blog/cli.rb
@@ -1,11 +1,22 @@
 require 'everything/blog'
+require 'logger'
 
 module Everything
   class Blog
     class CLI < Thor
+      class_option :verbose, type: :boolean, aliases: :v
+
       desc 'generate', 'generate an HTML site for the blog directory in your everything repo'
       def generate
+        logger.info("Generation of blog starting...")
         Everything::Blog.new.generate_site
+        logger.info("Generation of blog complete.")
+      end
+
+    private
+
+      def logger
+        @logger ||= Logger.new($stdout)
       end
     end
   end
--- spec/lib/everything/blog/cli_spec.rb
+++ spec/lib/everything/blog/cli_spec.rb
@@ -5,14 +5,78 @@ describe Everything::Blog::CLI do
     let(:given_cli_arguments) do
       ['generate']
     end
+
     let(:cli) do
-      described_class.start(given_cli_arguments)
+      described_class
+        .start(given_cli_arguments)
+    end
+    let(:fake_blog) do
+      double(Everything::Blog)
     end
 
-    it "calls blog's generate_site" do
-      expect_any_instance_of(Everything::Blog).to receive(:generate_site)
+    let(:fake_logger) do
+      Logger.new(fake_output)
+    end
+    let(:fake_output) do
+      StringIO.new
+    end
 
-      cli
+    before do
+      allow(Everything::Blog)
+        .to receive(:new)
+        .and_return(fake_blog)
+      allow(fake_blog)
+        .to receive(:generate_site)
+      allow(Logger)
+        .to receive(:new)
+        .and_return(fake_logger)
+    end
+
+    shared_context "calls blog's generate_site" do
+      it "calls blog's generate_site" do
+        cli
+
+        expect(fake_blog)
+          .to have_received(:generate_site)
+      end
+    end
+
+    context 'with no options' do
+      include_context "calls blog's generate_site"
+    end
+
+    context 'with an option of the' do
+      context 'short verbose flag' do
+        let(:given_cli_arguments) do
+          ['generate', '-v']
+        end
+
+        include_context "calls blog's generate_site"
+
+        it 'accepts a verbose flag and logs' do
+          expect_any_instance_of(Logger)
+            .to receive(:info)
+            .twice
+
+          cli
+        end
+      end
+
+      context 'long verbose flag' do
+        let(:given_cli_arguments) do
+          ['generate', '--verbose']
+        end
+
+        include_context "calls blog's generate_site"
+
+        it 'accepts a short verbose flag and logs' do
+          expect_any_instance_of(Logger)
+            .to receive(:info)
+            .twice
+
+          cli
+        end
+      end
     end
   end
 end
