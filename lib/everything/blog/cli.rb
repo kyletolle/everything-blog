39f8eab8c56d02b7f523e024e7a3ff27a8081b75
Kyle Tolle <kyle@nullsix.com>
2019-07-04 15:44:08 -0600

    Add some thoughts for how we might change logging

--- lib/everything/blog/cli.rb
+++ lib/everything/blog/cli.rb
@@ -4,6 +4,7 @@ require 'logger'
 module Everything
   class Blog
     class CLI < Thor
+      # TODO: Perhaps this should be in the blog class itself?
       LOGGER_INFO_STARTING = "Generation of blog starting..."
       LOGGER_INFO_COMPLETE = "Generation of blog complete."
 
@@ -11,10 +12,12 @@ module Everything
 
       desc 'generate', 'generate an HTML site for the blog directory in your everything repo'
       def generate
+        # TODO: Perhaps this logging should be in the blog class itself?
         logger.info(LOGGER_INFO_STARTING)
 
         blog.generate_site
 
+        # TODO: Perhaps this logging should be in the blog class itself?
         logger.info(LOGGER_INFO_COMPLETE)
       end
 
@@ -23,8 +26,24 @@ module Everything
       def blog
         Everything::Blog
           .new(logger: logger)
+        # TODO: We might only pass in a logging level like:
+        # def log_level
+        #   if options[:debug]
+        #     Logger::DEBUG
+        #   elsif options[:verbose]
+        #     Logger::INFO
+        #   else
+        #     Logger::ERROR
+        #   end
+        # end
+        #
+        # and then pass that log level to the blog object like
+        #
+        # Everything::Blog.new(log_level: log_level)
       end
 
+      # TODO: Maybe the logger should be pushed down into the blog class and
+      # then all this does is pass along whether it's in verbose mode?
       def logger
         @logger ||= Logger.new($stdout)
           .tap do |l|
@@ -34,6 +53,9 @@ module Everything
               else
                 Logger::ERROR
               end
+
+            # TODO: And then we might change how this log level setting works
+            # l.level = log_level
           end
       end
     end
