ee1c7271f2b4a38cf904c75ed66ffcd2adbfabc6
Kyle Tolle <kyle@nullsix.com>
2019-09-18 18:44:08 -0600

    Add a few more specs for logger refinement

--- spec/lib/everything/add_logger_to_everything_refinement_spec.rb
+++ spec/lib/everything/add_logger_to_everything_refinement_spec.rb
@@ -12,11 +12,42 @@ describe Everything do
         expect{ Everything.logger }.not_to raise_error
       end
 
-      it 'defaults to a logger', :aggregate_failures do
-        actual_logger = Everything.logger
-        expect(actual_logger).to be_a_kind_of(Logger)
-        expect(actual_logger.level).to eq(Logger::ERROR)
-        expect(actual_logger.progname).to eq(Module.to_s)
+      context 'when a logger was previously set' do
+        let(:test_logger) do
+          instance_double(Logger)
+        end
+
+        before do
+          Everything.logger = test_logger
+        end
+
+        it 'returns the previously given logger' do
+          actual_logger = Everything.logger
+          expect(actual_logger).to eq(test_logger)
+        end
+
+        it 'memoizes the value' do
+          first_logger = Everything.logger
+          second_logger = Everything.logger
+
+          expect(first_logger.object_id).to eq(second_logger.object_id)
+        end
+      end
+
+      context 'when a logger was not previously set' do
+        it 'defaults to a Logger', :aggregate_failures do
+          actual_logger = Everything.logger
+          expect(actual_logger).to be_a_kind_of(Logger)
+          expect(actual_logger.level).to eq(Logger::ERROR)
+          expect(actual_logger.progname).to eq(Module.to_s)
+        end
+
+        it 'memoizes the value' do
+          first_logger = Everything.logger
+          second_logger = Everything.logger
+
+          expect(first_logger.object_id).to eq(second_logger.object_id)
+        end
       end
     end
 
