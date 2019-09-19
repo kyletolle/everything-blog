8989d33a36ca62d4f2aa34b82e37a4df938c23fb
Kyle Tolle <kyle@nullsix.com>
2019-09-19 13:41:35 -0600

    Add more logger contexts

--- spec/lib/everything/add_logger_to_everything_refinement_spec.rb
+++ spec/lib/everything/add_logger_to_everything_refinement_spec.rb
@@ -12,41 +12,65 @@ describe Everything do
         expect{ Everything.logger }.not_to raise_error
       end
 
-      context 'when a logger was previously set' do
-        let(:test_logger) do
-          instance_double(Logger)
-        end
-
+      context 'when a logger is being overwritten' do
         before do
-          Everything.logger = test_logger
-        end
+          let(:original_logger) do
+            instance_double(Logger)
+          end
+          let(:new_logger) do
+            instance_double(Logger)
+          end
 
-        it 'returns the previously given logger' do
-          actual_logger = Everything.logger
-          expect(actual_logger).to eq(test_logger)
-        end
+          before do
+            Everything.logger = original_logger
+          end
 
-        it 'memoizes the value' do
-          first_logger = Everything.logger
-          second_logger = Everything.logger
-
-          expect(first_logger.object_id).to eq(second_logger.object_id)
+          it' is not the original logger' do
+            expect(Everything.logger).not_to eq(original_logger)
+          end
+          it 'is the new logger' do
+            expect(Everything.logger).to eq(new_logger)
+          end
         end
       end
 
-      context 'when a logger was not previously set' do
-        it 'defaults to a Logger', :aggregate_failures do
-          actual_logger = Everything.logger
-          expect(actual_logger).to be_a_kind_of(Logger)
-          expect(actual_logger.level).to eq(Logger::ERROR)
-          expect(actual_logger.progname).to eq(Module.to_s)
+      context 'when a logger is not being overwritten' do
+        context 'when a logger was previously set' do
+          let(:test_logger) do
+            instance_double(Logger)
+          end
+
+          before do
+            Everything.logger = test_logger
+          end
+
+          it 'returns the previously given logger' do
+            actual_logger = Everything.logger
+            expect(actual_logger).to eq(test_logger)
+          end
+
+          it 'memoizes the value' do
+            first_logger = Everything.logger
+            second_logger = Everything.logger
+
+            expect(first_logger.object_id).to eq(second_logger.object_id)
+          end
         end
 
-        it 'memoizes the value' do
-          first_logger = Everything.logger
-          second_logger = Everything.logger
+        context 'when a logger was not previously set' do
+          it 'defaults to a Logger', :aggregate_failures do
+            actual_logger = Everything.logger
+            expect(actual_logger).to be_a_kind_of(Logger)
+            expect(actual_logger.level).to eq(Logger::ERROR)
+            expect(actual_logger.progname).to eq(Module.to_s)
+          end
 
-          expect(first_logger.object_id).to eq(second_logger.object_id)
+          it 'memoizes the value' do
+            first_logger = Everything.logger
+            second_logger = Everything.logger
+
+            expect(first_logger.object_id).to eq(second_logger.object_id)
+          end
         end
       end
     end
