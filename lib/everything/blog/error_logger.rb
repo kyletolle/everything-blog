6a6ce5b98e1bc5b534d65c49931945e4a66f47e7
Kyle Tolle <kyle@nullsix.com>
2019-10-09 17:56:09 -0600

    Use iso8601 for error logger formatter

--- lib/everything/blog/error_logger.rb
+++ lib/everything/blog/error_logger.rb
@@ -3,7 +3,8 @@ module Everything
     class ErrorLogger < ::Logger
       DATETIME_PROGNAME_MESSAGE_FORMATTER =
         proc { |severity, datetime, progname, msg|
-          "#{datetime}: #{progname}: #{msg}\n"
+          iso8601_time = datetime.utc.iso8601
+          "#{iso8601_time}: #{progname}: #{msg}\n"
         }
 
       def initialize(logdev, progname: nil)
