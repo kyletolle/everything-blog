ea28af14f70e4446abfca6a9522b2fa520206689
Kyle Tolle <kyle@nullsix.com>
2019-10-09 18:15:03 -0600

    Remove unnecessary code from DebugLogger

--- lib/everything/blog/debug_logger.rb
+++ lib/everything/blog/debug_logger.rb
@@ -1,10 +1,6 @@
 module Everything
   class Blog
     class DebugLogger < Everything::Blog::Logger::Base
-      def initialize(logdev, progname: nil)
-        super
-        self.formatter = DATETIME_PROGNAME_MESSAGE_FORMATTER
-      end
     end
   end
 end
