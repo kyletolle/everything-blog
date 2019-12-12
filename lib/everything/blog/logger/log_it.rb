3b82f1d84b216e564f799717e2eb3622083bbd33
Kyle Tolle <kyle@nullsix.com>
2019-12-11 18:03:54 -0700

    Add a LogIt module

---
+++ lib/everything/blog/logger/log_it.rb
@@ -0,0 +1,28 @@
+module Everything
+  class Blog
+    class Logger
+      module LogIt
+        using Everything::AddLoggerToEverythingRefinement
+
+        def debug_it(message)
+          Everything.logger.debug(class_name) { message }
+        end
+
+        def error_it(message)
+          Everything.logger.error(class_name) { message }
+        end
+
+        def info_it(message)
+          Everything.logger.info(class_name) { message }
+        end
+
+      private
+
+        def class_name
+          self.class.to_s
+        end
+      end
+    end
+  end
+end
+
