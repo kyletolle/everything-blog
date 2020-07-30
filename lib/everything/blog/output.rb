fb854a4c788218d409c7ddd6e26840f562ac699b
Kyle Tolle <kyle@nullsix.com>
2020-07-30 00:58:02 -0600

    Add TODO for moving templates_dir to its own class

--- lib/everything/blog/output.rb
+++ lib/everything/blog/output.rb
@@ -6,6 +6,8 @@ module Everything
           Pathname.new(Fastenv.blog_output_path)
         end
 
+        # TODO: Let's create a Templates class that's at the same level as
+        # Output, and move this templates_dir functionality there...
         def templates_dir
           Pathname.new(Fastenv.templates_path)
         end
